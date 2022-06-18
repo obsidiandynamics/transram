62c2b4427fa474b33f3d9128c1fae88cb04f61af
Emil Koutanov <ekoutanov@gmail.com>
2022-06-18 16:28:36 +1000

    RunAllSpecs

---
+++ spec/src/main/java/com/obsidiandynamics/transram/run/RunAllSpecs.java
@@ -0,0 +1,8 @@
+package com.obsidiandynamics.transram.run;
+
+public class RunAllSpecs {
+  public static void main(String[] args) throws InterruptedException {
+    RunAllSrmlSpecs.main(args);
+    RunAllSs2plSpecs.main(args);
+  }
+}
